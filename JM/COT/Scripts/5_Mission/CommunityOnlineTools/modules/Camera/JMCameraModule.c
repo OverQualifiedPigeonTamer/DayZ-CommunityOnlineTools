08046093355c85c2c78df67664d964f0dee95af2
OverQualifiedPigeonTamer <overqualifiedpigeontamer@gmail.com>
2026-01-12 11:10:42 -0500

    Update JMCameraModule.c
    Remove Toggled Notification

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Camera/JMCameraModule.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Camera/JMCameraModule.c
@@ -579,7 +579,6 @@ Print("JMCameraModule::RPC_Leave_Finish - timestamp " + g_Game.GetTickTime());
 
 		if ( !GetCommunityOnlineToolsBase().IsActive() )
 		{
-			COTCreateLocalAdminNotification( new StringLocaliser( "STR_COT_NOTIFICATION_WARNING_TOGGLED_OFF" ) );
 			return;
 		}
 
